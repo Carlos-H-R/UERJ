5c3b67dc4268c3d746c0b13c70a7f87bc914c0df
Carlos-H-R <carlos.ribas.chr@gmail.com>
2024-11-30 17:45:02 -0300

    Adicionado Main

--- SO1/Corrida pelo tesouro/controler/client.py
+++ SO1/Corrida pelo tesouro/controler/client.py
@@ -1,10 +1,11 @@
 import socket
 
 
-class client:
+class Client:
     def __init__(self, IP, PORT) -> None:
         self.socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket_client.connect((IP,PORT))
 
 
-#
+if __name__ == "__main__":
+    c = Client(123, 8080)
